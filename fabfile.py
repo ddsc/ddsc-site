11a7f8cfeffac290e8cc2ea43aec61a06137d0c1
Reinout van Rees <reinout@vanrees.org>
2012-11-27 10:52:26 +0100

    Using latest fabric (including use_ssh_config option)

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,10 @@
+from fabric.api import env
+
 from sitesetup.fab.config import init_file
 from sitesetup.fab.tasks import *
+
+
+env.use_ssh_config = True
 
 # Most settings can be configured in fabfile.cfg
 init_file('fabfile.cfg')
