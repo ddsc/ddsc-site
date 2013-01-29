adb2a17f4e0719333f8634963883d49d8b851d7f
Roland van Laar <roland@micite.net>
2013-01-29 12:22:02 +0100

    Moved serializer code to serializer.

--- ddsc_site/views.py
+++ ddsc_site/views.py
@@ -2,68 +2,11 @@
 from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
 
-import json
-
-from rest_framework import generics, serializers
-
 from lizard_wms.models import WMSSource
 
+from rest_framework import generics
 
-class JSONField(serializers.Field):
-    """Get a Field that is in a JSONField in Django."""
-    def __init__(self, source, dict_name):
-        super(JSONField, self).__init__(source)
-        self.dict_name = dict_name
-
-    def field_to_native(self, obj, field_name):
-        """
-        Given an object and a field name, returns the value that should be
-        serialized for that field.
-        """
-        if obj is None:
-            return self.empty
-
-        data = getattr(obj, self.dict_name)
-        value = data.get(self.source, None)
-        return self.to_native(value)
-
-
-class WMSLayerSerializer(serializers.HyperlinkedModelSerializer):
-
-    url = serializers.HyperlinkedIdentityField(view_name='layers-detail')
-    search_url = serializers.HyperlinkedIdentityField(
-        view_name='layers-search')
-
-    styles = JSONField('styles', '_params')
-    format = JSONField('format', '_params')
-    height = JSONField('height', '_params')
-    width = JSONField('width', '_params')
-    tiled = JSONField('tiled', '_params')
-    transparent = JSONField('transparent', '_params')
-
-    opacity = serializers.SerializerMethodField('get_opacity')
-    type = serializers.SerializerMethodField('get_type')
-
-    wms_url = serializers.Field('url')
-
-    def get_wms_url(self, obj):
-        return obj.url
-
-    def get_opacity(self, obj):
-        options = obj.options
-        if isinstance(options, basestring):
-            options = json.loads(options)
-        return options['opacity']
-
-    def get_type(self, obj):
-        return 'wms'
-
-    class Meta:
-        model = WMSSource
-        fields = ('layer_name', 'display_name', 'url',
-                  'description', 'metadata', 'legend_url', 'enable_search',
-                  'styles', 'format', 'height', 'width', 'tiled',
-                  'transparent', 'wms_url', 'opacity', 'type', 'search_url')
+from .serializer import WMSLayerSerializer
 
 
 class LayerList(generics.ListCreateAPIView):
