98aceaf949c1f33d729363e4b6144051a2b3476c
Roland van Laar <roland@micite.net>
2013-01-29 12:29:24 +0100

    Added unicode methods.

--- ddsc_site/models.py
+++ ddsc_site/models.py
@@ -10,9 +10,15 @@ class Collage(models.Model):
 
     name = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.name
+
 
 class CollageItem(models.Model):
     """Collage Items."""
 
     name = models.CharField(max_length=100)
     collage = models.ForeignKey(Collage)
+
+    def __unicode__(self):
+        return '{} ({})'.format(self.name, self.collage.name)
