87fd1643c2b6379190fc2734d7af8db6f6a99d87
byrman <carsten.byrman@nelen-schuurmans.nl>
2013-03-08 13:40:38 +0100

    Make workspace items inline

--- ddsc_site/admin.py
+++ ddsc_site/admin.py
@@ -5,8 +5,19 @@ from django.contrib import admin
 
 from ddsc_site import models
 
+
+class WorkspaceItemInline(admin.TabularInline):
+    model = models.WorkspaceItem
+
+
+class WorkspaceAdmin(admin.ModelAdmin):
+    inlines = [
+        WorkspaceItemInline,
+    ]
+
+
 admin.site.register(models.Collage)
 admin.site.register(models.CollageItem)
-admin.site.register(models.Workspace)
+admin.site.register(models.Workspace, WorkspaceAdmin)
 admin.site.register(models.WorkspaceItem)
 admin.site.register(models.ProxyHostname)
