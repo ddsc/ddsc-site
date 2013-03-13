d395c5c4bf038f803befb17e9b28f6aa87544517
Berto Booijink <berto.booijink@nelen-schuurmans.nl>
2013-03-13 12:28:25 +0100

    Support urls without trailing slash.

--- ddsc_site/urls.py
+++ ddsc_site/urls.py
@@ -14,37 +14,37 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
 
-    url(r'^v0/collages/$', views.CollageList.as_view(), name='collage-list'),
-    url(r'^v0/collages/(?P<pk>\d+)/$', views.CollageDetail.as_view(),
+    url(r'^v0/collages/?$', views.CollageList.as_view(), name='collage-list'),
+    url(r'^v0/collages/(?P<pk>\d+)/?$', views.CollageDetail.as_view(),
         name='collage-detail'),
 
-    url(r'^v0/collageitems/$', views.CollageItemList.as_view(),
+    url(r'^v0/collageitems/?$', views.CollageItemList.as_view(),
         name='collageitem-list'),
-    url(r'^v0/collageitems/(?P<pk>\d+)/$',
+    url(r'^v0/collageitems/(?P<pk>\d+)/?$',
         views.CollageItemDetail.as_view(),
         name='collageitem-detail'),
 
-    url(r'^v0/workspaces/$', views.WorkspaceList.as_view(), name='workspace-list'),
-    url(r'^v0/workspaces/(?P<pk>\d+)/$', views.WorkspaceDetail.as_view(),
+    url(r'^v0/workspaces/?$', views.WorkspaceList.as_view(), name='workspace-list'),
+    url(r'^v0/workspaces/(?P<pk>\d+)/?$', views.WorkspaceDetail.as_view(),
         name='workspace-detail'),
 
-    url(r'^v0/workspaceitems/$', views.WorkspaceItemList.as_view(),
+    url(r'^v0/workspaceitems/?$', views.WorkspaceItemList.as_view(),
         name='workspaceitem-list'),
-    url(r'^v0/workspaceitems/(?P<pk>\d+)/$',
+    url(r'^v0/workspaceitems/(?P<pk>\d+)/?$',
         views.WorkspaceItemDetail.as_view(),
         name='workspaceitem-detail'),
 
-    url(r'^v0/layers/$', views.LayerList.as_view(), name='layer-list'),
-    url(r'^v0/layers/(?P<pk>\d+)/$', views.LayerDetail.as_view(),
+    url(r'^v0/layers/?$', views.LayerList.as_view(), name='layer-list'),
+    url(r'^v0/layers/(?P<pk>\d+)/?$', views.LayerDetail.as_view(),
         name='layer-detail'),
 
-    url(r'^v0/account/$', views.CurrentAccount.as_view(), name='account'),
-    url(r'^v0/account/login-url/$', LoginApiView.as_view(),
+    url(r'^v0/account/?$', views.CurrentAccount.as_view(), name='account'),
+    url(r'^v0/account/login-url/?$', LoginApiView.as_view(),
         name='ddsc_site.sso-login'),
-    url(r'^v0/account/logout-url/$', LogoutApiView.as_view(),
+    url(r'^v0/account/logout-url/?$', LogoutApiView.as_view(),
         name='ddsc_site.sso-logout'),
 
-    url(r'^v0/proxy/$', views.ProxyView.as_view(), name='proxy'),
+    url(r'^v0/proxy/?$', views.ProxyView.as_view(), name='proxy'),
 
     url(r'^', include('lizard_auth_client.urls')),
 )
