18651319af353b72a4e89000fb6a0f0dc689e767
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-04-06 03:06:21 +0200

    add csrf_exempt for annotation create view

--- ddsc_site/views.py
+++ ddsc_site/views.py
@@ -13,6 +13,7 @@ from django.views.decorators.gzip import gzip_page
 from django.views.generic import View
 from django.http import HttpResponse, HttpResponseForbidden
 from django.core.paginator import Paginator
+from django.views.decorators.csrf import csrf_exempt
 
 import requests
 from haystack.utils.geo import generate_bounding_box, Point
@@ -390,6 +391,10 @@ class AnnotationsCreateView(generics.CreateAPIView):
     model = Annotation
     serializer_class = serializers.AnnotationCreateSerializer
     permission_classes = (permissions.IsAuthenticated,)
+
+    @method_decorator(csrf_exempt)
+    def post(self, *args, **kwargs):
+        return super(AnnotationsCreateView, self).post(*args, **kwargs)
 
     def pre_save(self, obj):
         obj.username = self.request.user.username
