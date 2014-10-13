51587ce62413a2795ac0bdb0d1485a0a30ddcea2
fritzvd <f.vandeventer@gmail.com>
2014-10-13 15:28:01 +0200

    removed ts index

--- ddsc_site/search_indexes.py
+++ ddsc_site/search_indexes.py
@@ -2,9 +2,7 @@
 from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
 
-import re
 from haystack import indexes
-from ddsc_core.models import Timeseries
 from ddsc_site.models import Annotation
 
 
@@ -35,27 +33,3 @@ class AnnotationIndex(indexes.SearchIndex, indexes.Indexable):
 
     def get_updated_field(self):
         return 'updated_at'
-
-
-class TimeseriesIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-    name = indexes.CharField(model_attr='name', null=True)
-    location_name = indexes.CharField(model_attr='location__name', null=True)
-    location_geom = indexes.LocationField(
-        model_attr='location__real_geometry', null=True)
-    source = indexes.CharField(model_attr='source__name', null=True)
-
-    def get_model(self):
-        return Timeseries
-
-    def index_queryset(self, using=None):
-        return self.get_model().objects.all()
-
-    def get_updated_field(self):
-        return 'updated_at'
-
-    def prepare_name(self, obj):
-        return ' '.join(re.split(r'\W+|\_', obj.name))
-
-    def prepare_location_name(self, obj):
-        return ' '.join(re.split(r'\W+|\_', obj.location.name))
